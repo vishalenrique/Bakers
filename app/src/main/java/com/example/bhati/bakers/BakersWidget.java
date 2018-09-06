c0a91e4bedb9232e3636dd7e3dcb215b20345240
vishal <vishal3bhati@gmail.com>
2018-09-06 23:31:25 +0530

    Added widget functionality

--- app/src/main/java/com/example/bhati/bakers/BakersWidget.java
+++ app/src/main/java/com/example/bhati/bakers/BakersWidget.java
@@ -5,7 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 import com.google.gson.Gson;
@@ -25,6 +25,8 @@ public class BakersWidget extends AppWidgetProvider {
 
     private static List<Recipe> mRecipes;
     private static RemoteViews sViews;
+    private static int sSize;
+    private static int sCurrentIndex;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -36,24 +38,36 @@ public class BakersWidget extends AppWidgetProvider {
         getData(context);
 
 
-        Intent intent = new Intent(context,MainActivity.class);
+        Intent intent = new Intent(context, RecipeActivity.class);
+        intent.putExtra(RecipeFragment.EXTRA_RECIPE,mRecipes.get(sCurrentIndex));
         PendingIntent pendingIntent = PendingIntent.getActivity(
                 context,
                 0,
                 intent,
                 PendingIntent.FLAG_UPDATE_CURRENT
         );
-        sViews.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
+        sViews.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
-        Intent incrementIntent = new Intent(context,BakingWidgetService.class);
+        Intent incrementIntent = new Intent(context, BakingWidgetService.class);
         incrementIntent.setAction(BakingWidgetService.ACTION_INCREMENT);
         PendingIntent incrementPendingIntent = PendingIntent.getService(
                 context,
                 0,
-                intent,
+                incrementIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT
         );
-        sViews.setOnClickPendingIntent(R.id.widget_increment,incrementPendingIntent);
+        sViews.setOnClickPendingIntent(R.id.widget_increment, incrementPendingIntent);
+
+        Intent decrementIntent = new Intent(context, BakingWidgetService.class);
+        decrementIntent.setAction(BakingWidgetService.ACTION_DECREMENT);
+        PendingIntent decrementPendingIntent = PendingIntent.getService(
+                context,
+                0,
+                decrementIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        sViews.setOnClickPendingIntent(R.id.widget_decrement, decrementPendingIntent);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, sViews);
@@ -61,18 +75,40 @@ public class BakersWidget extends AppWidgetProvider {
 
     private static void getData(Context context) {
         deserialize(context);
-        Log.v("Widget",mRecipes.size()+"");
 
+        SharedPreferences sharedPreferences = context.getSharedPreferences(BakingWidgetService.PREF_BAKING, Context.MODE_PRIVATE);
+        sCurrentIndex = sharedPreferences.getInt(BakingWidgetService.PREF_CURRENT_INDEX, 0);
+
+        sSize = mRecipes.size();
+        sharedPreferences.edit().putInt(BakingWidgetService.PREF_SIZE, sSize).apply();
+
+
+        showContent(sCurrentIndex);
     }
 
     private static void deserialize(Context context) {
         Type recipeListType = new TypeToken<List<Recipe>>() {
         }.getType();
         mRecipes = new Gson().fromJson(loadJSONFromAsset(context), recipeListType);
-        StringBuilder builder = new StringBuilder(mRecipes.get(0).getName()+"\n\n");
-//        for(Ingredient ingredient:mRecipes.get(0).getIngredients()){
-//            builder.append(ingredient.getIngredient()+"\n");
-//        }
+    }
+
+    private static void showContent(int index) {
+        StringBuilder builder = new StringBuilder();
+        for (Ingredient ingredient : mRecipes.get(index).getIngredients()) {
+            builder.append("\u2022 ")
+                    .append(ingredient.getIngredient())
+                    .append(", ")
+                    .append(ingredient.getQuantity())
+                    .append(" ")
+                    .append(ingredient.getMeasure());
+
+            if(ingredient.getQuantity()>1){
+                builder.append("S");
+            }
+
+            builder.append("\n");
+        }
+        sViews.setTextViewText(R.id.appwidget_recipe_name, mRecipes.get(index).getName());
         sViews.setTextViewText(R.id.appwidget_text, builder.toString());
     }
 
@@ -95,6 +131,12 @@ public class BakersWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    public static void updateFromService(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
--- app/src/main/java/com/example/bhati/bakers/BakingWidgetService.java
+++ app/src/main/java/com/example/bhati/bakers/BakingWidgetService.java
@@ -1,7 +1,14 @@
 package com.example.bhati.bakers;
 
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
@@ -9,7 +16,13 @@ import android.widget.Toast;
 public class BakingWidgetService extends IntentService {
 
     public static final String ACTION_INCREMENT = "com.example.bhati.bakers.action.increment";
+    public static final String ACTION_DECREMENT = "com.example.bhati.bakers.action.decrement";
     private static final String TAG = BakingWidgetService.class.getSimpleName();
+    public static final String PREF_BAKING = "preferences";
+    public static final String PREF_SIZE = "totalSize";
+    public static final String PREF_CURRENT_INDEX = "currentIndex";
+    private int mSize;
+    private int mCurrentIndex;
 
     public BakingWidgetService() {
         super("BakingWidgetService");
@@ -19,9 +32,65 @@ public class BakingWidgetService extends IntentService {
     protected void onHandleIntent(@Nullable Intent intent) {
         if (intent != null) {
             final String action = intent.getAction();
-            if(ACTION_INCREMENT.equals(action)){
-                Toast.makeText(this, "onHandleIntent called", Toast.LENGTH_SHORT).show();
+            if (ACTION_INCREMENT.equals(action)) {
+                increment();
+            } else if (ACTION_DECREMENT.equals((action))) {
+                decrement();
             }
         }
+    }
+
+    private void decrement() {
+        SharedPreferences sharedPreferences = getValuesFromSharedPreferences();
+        if (mCurrentIndex == 0) {
+            Log.v(TAG, getString(R.string.first_item));
+            showToast(getString(R.string.first_item));
+            return;
+        }
+        mCurrentIndex--;
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(PREF_CURRENT_INDEX, mCurrentIndex);
+        editor.apply();
+        updateWidget();
+        Log.v(TAG, "onHandleIntent with decrement action, called");
+    }
+
+    @NonNull
+    private SharedPreferences getValuesFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF_BAKING, Context.MODE_PRIVATE);
+        mSize = sharedPreferences.getInt(BakingWidgetService.PREF_SIZE, 0);
+        mCurrentIndex = sharedPreferences.getInt(BakingWidgetService.PREF_CURRENT_INDEX, 0);
+        return sharedPreferences;
+    }
+
+    private void increment() {
+        SharedPreferences sharedPreferences = getValuesFromSharedPreferences();
+        if (mCurrentIndex == (mSize - 1)) {
+            Log.v(TAG, getString(R.string.last_item));
+            showToast(getString(R.string.last_item));
+            return;
+        }
+        mCurrentIndex++;
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(PREF_CURRENT_INDEX, mCurrentIndex);
+        editor.apply();
+        updateWidget();
+        Log.v(TAG, "onHandleIntent with increment action, called");
+    }
+
+    private void showToast(final String message) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(BakingWidgetService.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateWidget() {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, BakersWidget.class));
+        BakersWidget.updateFromService(this, appWidgetManager, appWidgetIds);
     }
 }
